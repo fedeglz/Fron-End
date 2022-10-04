5f178a704a9fe35dac6efa92cd528c33e10dd782
federico González <fedeg130@gmail.com>
2022-10-04 11:14:30 -0300

    codificacion netBeans

---
+++ TPfinal/BackEnd/src/main/java/com/portfolio/fg/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+package com.portfolio.fg.Repository;
+
+import com.portfolio.fg.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
