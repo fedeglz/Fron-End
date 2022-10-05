d173ee5c815c97125a907c5fe155a5a5acd0efc8
federico González <fedeg130@gmail.com>
2022-10-05 02:38:17 -0300

    Implementacion en el FronEnd

--- TPfinal/BackEnd/src/main/java/com/portfolio/fg/Controller/PersonaController.java
+++ TPfinal/BackEnd/src/main/java/com/portfolio/fg/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.fg.Entity.Persona;
 import com.portfolio.fg.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -30,7 +32,7 @@ public class PersonaController {
         return "La persona fue creada correctamente";
     }
     
-    @DeleteMapping("/persona/borrar/{id}")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletPersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
@@ -54,6 +56,12 @@ public class PersonaController {
     
     }
     
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)9);
+    }
     
- 
+    
+    
+       
 }
