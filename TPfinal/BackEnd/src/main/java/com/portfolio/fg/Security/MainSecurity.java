eff7614f4085ba72a6b63f488294deacb8c0bd25
federico González <fedeg130@gmail.com>
2022-10-12 01:20:27 -0300

    login con JWT (backend)

---
+++ TPfinal/BackEnd/src/main/java/com/portfolio/fg/Security/MainSecurity.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.fg.Security;
+
+import com.portfolio.fg.Security.Service.UserDetailsImpl;
+import com.portfolio.fg.Security.jwt.JwtEntryPoint;
+import com.portfolio.fg.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsImpl userDetailsServicesImpl;
+    @Autowired
+    JwtEntryPoint jwtEntrypoint;
+    
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+    
+    
+    @Bean
+    public PasswordEncoder passwordEncored(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.cors().and().csrf().disable()
+               .authorizeRequests()
+               .antMatchers("/auth/**").permitAll()
+               .anyRequest().authenticated()
+               .and()
+               .exceptionHandling().authenticationEntryPoint(jwtEntrypoint)
+               .and()
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+               
+              
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServicesImpl)
+                .passwordEncoder(passwordEncored());
+    }
+    
+}
---
+++ TPfinal/BackEnd/src/main/java/com/portfolio/fg/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.fg.Security.Repository;
+
+import com.portfolio.fg.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
+    Optional<Usuario> findByNombreUsuario(String nomnreUsuario);
+    
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+    
+    
+}
